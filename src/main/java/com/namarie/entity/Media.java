09539c9eeb67a779919a01acb6e78e8d422ce278
Francsico Dueñas <frsduenasca@unal.edu.co>
2022-07-07 21:31:43 -0500

    :zap: Song class inherits from media class

---
+++ src/main/java/com/namarie/entity/Media.java
@@ -0,0 +1,26 @@
+package com.namarie.entity;
+
+public class Media {
+
+    private final int number;
+    private final String name;
+
+    public Media(int number, String name) {
+        this.number = number;
+        this.name = name;
+    }
+
+    public int getNumber() {
+        return number;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    @Override
+    public String toString() {
+        return String.format(" %05d %s ", getNumber(), getName().substring(0, getName().length() - 4));
+    }
+
+}
---
+++ src/main/java/com/namarie/entity/Song.java
@@ -0,0 +1,26 @@
+package com.namarie.entity;
+
+public class Song extends Media {
+
+    private final String singer;
+    private final String gender;
+
+    public Song(int number, String name, String singer, String gender) {
+        super(number, name);
+        this.singer = singer;
+        this.gender = gender;
+    }
+
+    public String getSinger() {
+        return singer;
+    }
+
+    public String getGender() {
+        return gender;
+    }
+
+    @Override
+    public String toString() {
+        return String.format(" %05d %s %s", getNumber(), getSinger(), getName().substring(0, getName().length() - 4));
+    }
+}
