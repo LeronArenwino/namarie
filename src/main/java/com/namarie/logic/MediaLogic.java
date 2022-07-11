dee9627c942572db55aff6119a4238fe51c5833b
Francisco Dueñas <frsduenasca@unal.edu.co>
2022-07-11 12:37:47 -0500

    :sparkles: New MediaLogic class

---
+++ src/main/java/com/namarie/logic/MediaLogic.java
@@ -0,0 +1,10 @@
+package com.namarie.logic;
+
+/** This class control the logic of the application relative with media, like generate music list, video list, genders list, etc.
+ * @author Francisco Dueñas
+ */
+public class MediaLogic {
+
+
+
+}
