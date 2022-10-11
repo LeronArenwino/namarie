3ab099d5fbeda6abcdb3c3cb84a12d047a0df205
Francisco Dueñas <frsduenasca@unal.edu.co>
2022-10-11 10:23:21 -0500

    :fire: Remove useless code

--- src/main/java/com/namarie/gui/SettingsWindow.java
+++ src/main/java/com/namarie/gui/SettingsWindow.java
@@ -114,97 +114,6 @@ public class SettingsWindow extends JFrame {
 
         initComponents();
 
-        this.addWindowListener(new WindowListener() {
-
-
-            /**
-             * Invoked the first time a window is made visible.
-             *
-             * @param e
-             */
-            @Override
-            public void windowOpened(WindowEvent e) {
-
-            }
-
-            /**
-             * Invoked when the user attempts to close the window
-             * from the window's system menu.
-             *
-             * @param e
-             */
-            @Override
-            public void windowClosing(WindowEvent e) {
-
-            }
-
-            /**
-             * Invoked when a window has been closed as the result
-             * of calling dispose on the window.
-             *
-             * @param e
-             */
-            @Override
-            public void windowClosed(WindowEvent e) {
-                MainWindow.timerFocusMainPanel.start();
-            }
-
-            /**
-             * Invoked when a window is changed from a normal to a
-             * minimized state. For many platforms, a minimized window
-             * is displayed as the icon specified in the window's
-             * iconImage property.
-             *
-             * @param e
-             * @see Frame#setIconImage
-             */
-            @Override
-            public void windowIconified(WindowEvent e) {
-
-            }
-
-            /**
-             * Invoked when a window is changed from a minimized
-             * to a normal state.
-             *
-             * @param e
-             */
-            @Override
-            public void windowDeiconified(WindowEvent e) {
-
-            }
-
-            /**
-             * Invoked when the Window is set to be the active Window. Only a Frame or
-             * a Dialog can be the active Window. The native windowing system may
-             * denote the active Window or its children with special decorations, such
-             * as a highlighted title bar. The active Window is always either the
-             * focused Window, or the first Frame or Dialog that is an owner of the
-             * focused Window.
-             *
-             * @param e
-             */
-            @Override
-            public void windowActivated(WindowEvent e) {
-
-            }
-
-            /**
-             * Invoked when a Window is no longer the active Window. Only a Frame or a
-             * Dialog can be the active Window. The native windowing system may denote
-             * the active Window or its children with special decorations, such as a
-             * highlighted title bar. The active Window is always either the focused
-             * Window, or the first Frame or Dialog that is an owner of the focused
-             * Window.
-             *
-             * @param e
-             */
-            @Override
-            public void windowDeactivated(WindowEvent e) {
-
-            }
-        });
-
         saveButton.addActionListener(new ActionListener() {
             /**
              * Invoked when an action occurs.
@@ -492,9 +401,10 @@ public class SettingsWindow extends JFrame {
         this.setMinimumSize(new Dimension(RESOLUTION_WIDTH / 2, RESOLUTION_HEIGHT / 2));
         this.setLocationRelativeTo(null);
         this.setResizable(false);
-        this.setDefaultCloseOperation(WindowConstants.DISPOSE_ON_CLOSE);
+        this.setDefaultCloseOperation(WindowConstants.HIDE_ON_CLOSE);
         this.getContentPane().setLayout(new BorderLayout(0, 0));
         this.getContentPane().add(containerPanel);
+        this.setAlwaysOnTop(true);
         this.setVisible(false);
 
         loadSettings(SettingsLogic.loadSettings());
