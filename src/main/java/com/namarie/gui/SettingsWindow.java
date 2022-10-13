fe5e373d3747e32061a009730fda7dde2128efaa
Francisco Dueñas <francisco.duenas@ikusi.com>
2022-10-12 22:16:29 -0500

    :zap: Using timer

--- src/main/java/com/namarie/gui/SettingsWindow.java
+++ src/main/java/com/namarie/gui/SettingsWindow.java
@@ -109,6 +109,12 @@ public class SettingsWindow extends JFrame {
     private JLabel fontStyleLabel;
     private JComboBox<String> fontStyleComboBox;
     private JButton exitButton;
+    private JButton button1;
+    private JButton button2;
+    private JButton button3;
+    private JButton button4;
+    private JButton button5;
+    private JButton button6;
 
     public SettingsWindow() {
 
@@ -397,11 +403,100 @@ public class SettingsWindow extends JFrame {
 
     private void initComponents() {
 
+        this.addWindowListener(new WindowListener() {
+
+
+            /**
+             * Invoked the first time a window is made visible.
+             *
+             * @param e
+             */
+            @Override
+            public void windowOpened(WindowEvent e) {
+            }
+
+            /**
+             * Invoked when the user attempts to close the window
+             * from the window's system menu.
+             *
+             * @param e
+             */
+            @Override
+            public void windowClosing(WindowEvent e) {
+            }
+
+            /**
+             * Invoked when a window has been closed as the result
+             * of calling dispose on the window.
+             *
+             * @param e
+             */
+            @Override
+            public void windowClosed(WindowEvent e) {
+                MainWindow.timerFocusMainPanel.start();
+            }
+
+            /**
+             * Invoked when a window is changed from a normal to a
+             * minimized state. For many platforms, a minimized window
+             * is displayed as the icon specified in the window's
+             * iconImage property.
+             *
+             * @param e
+             * @see Frame#setIconImage
+             */
+            @Override
+            public void windowIconified(WindowEvent e) {
+
+            }
+
+            /**
+             * Invoked when a window is changed from a minimized
+             * to a normal state.
+             *
+             * @param e
+             */
+            @Override
+            public void windowDeiconified(WindowEvent e) {
+
+            }
+
+            /**
+             * Invoked when the Window is set to be the active Window. Only a Frame or
+             * a Dialog can be the active Window. The native windowing system may
+             * denote the active Window or its children with special decorations, such
+             * as a highlighted title bar. The active Window is always either the
+             * focused Window, or the first Frame or Dialog that is an owner of the
+             * focused Window.
+             *
+             * @param e
+             */
+            @Override
+            public void windowActivated(WindowEvent e) {
+
+            }
+
+            /**
+             * Invoked when a Window is no longer the active Window. Only a Frame or a
+             * Dialog can be the active Window. The native windowing system may denote
+             * the active Window or its children with special decorations, such as a
+             * highlighted title bar. The active Window is always either the focused
+             * Window, or the first Frame or Dialog that is an owner of the focused
+             * Window.
+             *
+             * @param e
+             */
+            @Override
+            public void windowDeactivated(WindowEvent e) {
+
+            }
+        });
+
         this.setTitle("Settings");
         this.setMinimumSize(new Dimension(RESOLUTION_WIDTH / 2, RESOLUTION_HEIGHT / 2));
         this.setLocationRelativeTo(null);
         this.setResizable(false);
-        this.setDefaultCloseOperation(WindowConstants.HIDE_ON_CLOSE);
+        this.setDefaultCloseOperation(WindowConstants.DISPOSE_ON_CLOSE);
         this.getContentPane().setLayout(new BorderLayout(0, 0));
         this.getContentPane().add(containerPanel);
         this.setVisible(false);
