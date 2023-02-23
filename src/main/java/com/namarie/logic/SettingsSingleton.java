a03cdcf4f17d4988b9df74b3cce5695b7b5923ff
Francisco Dueñas <frsduenasca@unal.edu.co>
2023-02-22 22:35:38 -0500

    :zap: [NEW] Class to manage settings window

---
+++ src/main/java/com/namarie/logic/SettingsSingleton.java
@@ -0,0 +1,278 @@
+package com.namarie.logic;
+
+import java.awt.*;
+import java.util.Properties;
+
+public class SettingsSingleton {
+
+    private static final SettingsSingleton instance;
+
+    private SettingsSingleton() {
+    }
+
+    // Screen resolution
+    public static final GraphicsDevice RESOLUTION = GraphicsEnvironment.getLocalGraphicsEnvironment().getDefaultScreenDevice();
+    public static final int RESOLUTION_WIDTH = RESOLUTION.getDisplayMode().getWidth();
+    public static final int RESOLUTION_HEIGHT = RESOLUTION.getDisplayMode().getHeight();
+
+    // Path to default settings file
+    public static final String PATH_TO_SETTINGS = "settings";
+
+    // Folders TabPanel
+    public static final String KEY_PATH_TO_VIDEOS = "pathToVideos";
+    public static final String KEY_PATH_TO_SONGS = "pathToSongs";
+    public static final String KEY_IS_PROMOTIONAL_VIDEOS = "isPromotionalVideos";
+    public static final String KEY_PATH_TO_PROMOTIONAL_VIDEOS = "pathToPromotionalVideos";
+
+    // Time TabPanel
+    public static final String KEY_TIME_TO_PLAY_RANDOM_SONGS = "timeToPlayRandomSongs";
+    public static final String KEY_TIME_TO_REPEAT_SONGS = "timeToRepeatSongs";
+
+    // Credits TabPanel
+    public static final String KEY_AMOUNT_CREDITS = "amountCredits";
+    public static final String KEY_LOCK_SCREEN = "lockScreen";
+    public static final String KEY_SAVE_SONGS = "saveSongs";
+
+    // Keys TabPanel
+    public static final String KEY_UP_SONG = "upSong";
+    public static final String KEY_DOWN_SONG = "downSong";
+    public static final String KEY_UP_SONGS = "upSongs";
+    public static final String KEY_DOWN_SONGS = "downSongs";
+    public static final String KEY_UP_GENDER = "upGender";
+    public static final String KEY_DOWN_GENDER = "downGender";
+    public static final String KEY_ADD_COIN = "addCoin";
+    public static final String KEY_REMOVE_COIN = "removeCoin";
+    public static final String KEY_POWER_OFF = "powerOff";
+    public static final String KEY_NEXT_SONG = "nextSong";
+    public static final String KEY_SETTINGS = "settings";
+
+    //View TabPanel
+//    public static final String KEY_BACKGROUND_COLOR = "backgroundColor";
+//    public static final String KEY_TEXT_COLOR = "textColor";
+//    public static final String KEY_FONT = "font";
+//    public static final String KEY_FONT_STYLE = "fontStyle";
+//    public static final String KEY_FOREGROUND = "foreground";
+//    public static final String KEY_FONT_SIZE = "fontSize";
+//    public static final String KEY_BOLD = "fontBold";
+
+    // Folders constants
+    private static final String DEFAULT_VALUE_PATH_VIDEOS = "";
+    private static final String DEFAULT_VALUE_PATH_SONGS = "";
+    private static final String DEFAULT_VALUE_PROMOTIONAL_VIDEO = "false";
+    private static final String DEFAULT_VALUE_PATH_PROMOTIONAL_VIDEO = "";
+
+    //Time constants
+    private static final String DEFAULT_VALUE_RANDOM_SONG = "1";
+    private static final String DEFAULT_VALUE_REPEAT_SONGS = "5";
+
+    //Credits constants
+    private static final String DEFAULT_VALUE_AMOUNT_CREDITS = "1";
+    private static final String DEFAULT_VALUE_LOCK_SCREEN = "false";
+    private static final String DEFAULT_VALUE_SAVE_SONGS = "false";
+
+    //Keys constants
+    private static final String DEFAULT_VALUE_UP_SONG = "38";
+    private static final String DEFAULT_VALUE_DOWN_SONG = "40";
+    private static final String DEFAULT_VALUE_UP_SONGS = "106";
+    private static final String DEFAULT_VALUE_DOWN_SONGS = "111";
+    private static final String DEFAULT_VALUE_UP_GENDER = "107";
+    private static final String DEFAULT_VALUE_DOWN_GENDER = "109";
+    private static final String DEFAULT_VALUE_ADD_COIN = "65";
+    private static final String DEFAULT_VALUE_REMOVE_COIN = "66";
+    private static final String DEFAULT_VALUE_NEXT_SONG = "78";
+    private static final String DEFAULT_VALUE_POWER_OFF = "70";
+    private static final String DEFAULT_VALUE_SETTINGS = "67";
+
+    //View constants
+//    private static final String DEFAULT_VALUE_BACKGROUND_COLOR = "102,204,255";
+//    private static final String DEFAULT_VALUE_TEXT_COLOR = "255,255,255";
+//    private static final String DEFAULT_VALUE_FONT = "Arial";
+//    private static final String DEFAULT_VALUE_FONT_STYLE = "Regular";
+//    private static final String DEFAULT_VALUE_FONT_SIZE = "20";
+//    private static final String DEFAULT_VALUE_FOREGROUND = "000,000,000";
+//    private static final String DEFAULT_VALUE_BOLD = "false";
+
+    // Paths control variables
+    public static String pathToVideos;
+    public static String pathToSongs;
+    public static boolean isPromotionalVideos;
+    public static String pathToPromotionalVideos;
+
+    // Time control variables
+    public static int timeToPlayRandomSongs;
+    public static int timeToRepeatSongs;
+
+    // Credits control variables
+    public static int amountCredits;
+    public static boolean isLockScreen;
+    public static boolean isSaveSongs;
+
+    // Keys control variables
+    public static int valueToUpIndex;
+    public static int valueToDownIndex;
+    public static int valueToUpIndexes;
+    public static int valueToDownIndexes;
+    public static int valueToChangeGenderToUp;
+    public static int valueToChangeGenderToDown;
+    public static int valueToAddCoin;
+    public static int valueToRemoveCoin;
+    public static int valueToPlayNextSong;
+    public static int valueToPowerOff;
+
+    static {
+        try {
+            instance = new SettingsSingleton();
+        } catch (Exception e) {
+            throw new RuntimeException("Exception occurred in creating singleton instance");
+        }
+    }
+
+    @SuppressWarnings("UnusedReturnValue")
+    public static SettingsSingleton getInstance(){
+        return instance;
+    }
+
+    public static Properties defaultSettingsProperties(){
+
+        Properties properties = new Properties();
+
+        properties.put(KEY_PATH_TO_VIDEOS, DEFAULT_VALUE_PATH_VIDEOS);
+        properties.put(KEY_PATH_TO_SONGS, DEFAULT_VALUE_PATH_SONGS);
+        properties.put(KEY_IS_PROMOTIONAL_VIDEOS, DEFAULT_VALUE_PROMOTIONAL_VIDEO);
+        properties.put(KEY_PATH_TO_PROMOTIONAL_VIDEOS, DEFAULT_VALUE_PATH_PROMOTIONAL_VIDEO);
+
+        //Time
+        properties.put(KEY_TIME_TO_PLAY_RANDOM_SONGS, DEFAULT_VALUE_RANDOM_SONG);
+        properties.put(KEY_TIME_TO_REPEAT_SONGS, DEFAULT_VALUE_REPEAT_SONGS);
+
+        //Credits
+        properties.put(KEY_AMOUNT_CREDITS, DEFAULT_VALUE_AMOUNT_CREDITS);
+        properties.put(KEY_LOCK_SCREEN, DEFAULT_VALUE_LOCK_SCREEN);
+        properties.put(KEY_SAVE_SONGS, DEFAULT_VALUE_SAVE_SONGS);
+
+        //Keys
+        properties.put(KEY_UP_SONG, DEFAULT_VALUE_UP_SONG);
+        properties.put(KEY_DOWN_SONG, DEFAULT_VALUE_DOWN_SONG);
+        properties.put(KEY_UP_SONGS, DEFAULT_VALUE_UP_SONGS);
+        properties.put(KEY_DOWN_SONGS, DEFAULT_VALUE_DOWN_SONGS);
+        properties.put(KEY_UP_GENDER, DEFAULT_VALUE_UP_GENDER);
+        properties.put(KEY_DOWN_GENDER, DEFAULT_VALUE_DOWN_GENDER);
+        properties.put(KEY_ADD_COIN, DEFAULT_VALUE_ADD_COIN);
+        properties.put(KEY_REMOVE_COIN, DEFAULT_VALUE_REMOVE_COIN);
+        properties.put(KEY_NEXT_SONG, DEFAULT_VALUE_NEXT_SONG);
+        properties.put(KEY_POWER_OFF, DEFAULT_VALUE_POWER_OFF);
+        properties.put(KEY_SETTINGS, DEFAULT_VALUE_SETTINGS);
+
+        return properties;
+
+    }
+
+    public static void setSettingsFromProperties(Properties properties){
+
+        // Set paths control variables
+        setPathToVideos(properties.getProperty(KEY_PATH_TO_VIDEOS));
+        setPathToSongs(properties.getProperty(KEY_PATH_TO_SONGS));
+        setIsPromotionalVideos(Boolean.parseBoolean(properties.getProperty(KEY_IS_PROMOTIONAL_VIDEOS)));
+        setPathToPromotionalVideos(properties.getProperty(KEY_PATH_TO_PROMOTIONAL_VIDEOS));
+
+        // Set time control variables
+        setTimeToPlayRandomSongs(Integer.parseInt(properties.getProperty(KEY_TIME_TO_PLAY_RANDOM_SONGS)));
+        setTimeToRepeatSongs(Integer.parseInt(properties.getProperty(KEY_TIME_TO_REPEAT_SONGS)));
+
+        // Set credits control variables
+        setAmountCredits(Integer.parseInt(properties.getProperty(KEY_AMOUNT_CREDITS)));
+        setIsLockScreen(Boolean.parseBoolean(properties.getProperty(KEY_LOCK_SCREEN)));
+        setIsSaveSongs(Boolean.parseBoolean(properties.getProperty(KEY_SAVE_SONGS)));
+
+        // Set keys control variables
+        setValueToUpIndex(Integer.parseInt(properties.getProperty(KEY_UP_SONG)));
+        setValueToDownIndex(Integer.parseInt(properties.getProperty(KEY_DOWN_SONG)));
+        setValueToUpIndexes(Integer.parseInt(properties.getProperty(KEY_UP_SONGS)));
+        setValueToDownIndexes(Integer.parseInt(properties.getProperty(KEY_DOWN_SONGS)));
+        setValueToChangeGenderToUp(Integer.parseInt(properties.getProperty(KEY_UP_GENDER)));
+        setValueToChangeGenderToDown(Integer.parseInt(properties.getProperty(KEY_DOWN_GENDER)));
+        setValueToAddCoin(Integer.parseInt(properties.getProperty(KEY_ADD_COIN)));
+        setValueToRemoveCoin(Integer.parseInt(properties.getProperty(KEY_REMOVE_COIN)));
+        setValueToPlayNextSong(Integer.parseInt(properties.getProperty(KEY_NEXT_SONG)));
+        setValueToPowerOff(Integer.parseInt(properties.getProperty(KEY_POWER_OFF)));
+        // setValueToSettingsWindow(Integer.valueOf(properties.getProperty(KEY_SETTINGS)));
+
+    }
+
+    private static void setPathToVideos(String pathToVideos) {
+        SettingsSingleton.pathToVideos = pathToVideos;
+    }
+
+    private static void setPathToSongs(String pathToSongs) {
+        SettingsSingleton.pathToSongs = pathToSongs;
+    }
+
+    private static void setIsPromotionalVideos(boolean isPromotionalVideos) {
+        SettingsSingleton.isPromotionalVideos = isPromotionalVideos;
+    }
+
+    private static void setPathToPromotionalVideos(String pathToPromotionalVideos) {
+        SettingsSingleton.pathToPromotionalVideos = pathToPromotionalVideos;
+    }
+
+    private static void setTimeToPlayRandomSongs(int timeToPlayRandomSongs) {
+        SettingsSingleton.timeToPlayRandomSongs = timeToPlayRandomSongs;
+    }
+
+    private static void setTimeToRepeatSongs(int timeToRepeatSongs) {
+        SettingsSingleton.timeToRepeatSongs = timeToRepeatSongs;
+    }
+
+    private static void setAmountCredits(int amountCredits) {
+        SettingsSingleton.amountCredits = amountCredits;
+    }
+
+    private static void setIsLockScreen(boolean isLockScreen) {
+        SettingsSingleton.isLockScreen = isLockScreen;
+    }
+
+    private static void setIsSaveSongs(boolean isSaveSongs) {
+        SettingsSingleton.isSaveSongs = isSaveSongs;
+    }
+
+    private static void setValueToUpIndex(int valueToUpIndex) {
+        SettingsSingleton.valueToUpIndex = valueToUpIndex;
+    }
+
+    private static void setValueToDownIndex(int valueToDownIndex) {
+        SettingsSingleton.valueToDownIndex = valueToDownIndex;
+    }
+
+    private static void setValueToUpIndexes(int valueToUpIndexes) {
+        SettingsSingleton.valueToUpIndexes = valueToUpIndexes;
+    }
+
+    private static void setValueToDownIndexes(int valueToDownIndexes) {
+        SettingsSingleton.valueToDownIndexes = valueToDownIndexes;
+    }
+
+    private static void setValueToChangeGenderToUp(int valueToChangeGenderToUp) {
+        SettingsSingleton.valueToChangeGenderToUp = valueToChangeGenderToUp;
+    }
+
+    private static void setValueToChangeGenderToDown(int valueToChangeGenderToDown) {
+        SettingsSingleton.valueToChangeGenderToDown = valueToChangeGenderToDown;
+    }
+
+    private static void setValueToAddCoin(int valueToAddCoin) {
+        SettingsSingleton.valueToAddCoin = valueToAddCoin;
+    }
+
+    private static void setValueToRemoveCoin(int valueToRemoveCoin) {
+        SettingsSingleton.valueToRemoveCoin = valueToRemoveCoin;
+    }
+
+    private static void setValueToPlayNextSong(int valueToPlayNextSong) {
+        SettingsSingleton.valueToPlayNextSong = valueToPlayNextSong;
+    }
+
+    private static void setValueToPowerOff(int valueToPowerOff) {
+        SettingsSingleton.valueToPowerOff = valueToPowerOff;
+    }
+
+}
