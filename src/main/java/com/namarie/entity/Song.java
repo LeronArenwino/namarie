84e4ca0c6c178a4145bb39c70981c67c3bb012d5
Francisco Dueñas <frsduenasca@unal.edu.co>
2023-03-17 16:54:15 -0500

    :zap: [UPDATE] FORMAT_SONG_LIST constant

--- src/main/java/com/namarie/entity/Song.java
+++ src/main/java/com/namarie/entity/Song.java
@@ -3,9 +3,9 @@ package com.namarie.entity;
 import java.io.File;
 import java.util.Optional;
 
-import static com.namarie.logic.SettingsSingleton.ACTION_SONG;
-
 public class Song extends Multimedia {
+
+    private static final String FORMAT_SONG_LIST = "%s%s%s%s%s%s%s";
 
     private final String singer;
     private final String gender;
@@ -26,7 +26,7 @@ public class Song extends Multimedia {
 
     public Optional<String> pathToSong(String path){
 
-        String pathToSong = String.format(ACTION_SONG, path, File.separator, getGender(), File.separator, getSinger(), File.separator, getName());
+        String pathToSong = String.format(FORMAT_SONG_LIST, path, File.separator, getGender(), File.separator, getSinger(), File.separator, getName());
 
         return Optional.ofNullable(pathToSong);
 
