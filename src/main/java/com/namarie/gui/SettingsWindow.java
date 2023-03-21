954e1974493c2800130afc1ca1d10c6efd734e5b
Francisco Dueñas <frsduenasca@unal.edu.co>
2023-03-21 12:57:06 -0500

    :fire: [DELETE] Remove useless variables

--- src/main/java/com/namarie/gui/SettingsWindow.java
+++ src/main/java/com/namarie/gui/SettingsWindow.java
@@ -11,6 +11,9 @@ import static com.namarie.logic.SettingsSingleton.*;
 
 public class SettingsWindow extends JFrame {
 
+    // Fonts
+    protected static final String[] fontFamilyNames = graphicsEnvironment.getAvailableFontFamilyNames();
+
     private JPanel containerPanel;
     private JPanel centerPanel;
     private JTabbedPane containerTabbedPane;
@@ -18,13 +21,11 @@ public class SettingsWindow extends JFrame {
     private JPanel foldersPanel;
     private JPanel timePanel;
     private JPanel creditsPanel;
-    private JPanel keysPanel;
     private JPanel mainViewPanel;
     private JLabel titleWelcomePanel;
     private JLabel titleFoldersPanel;
     private JLabel titleTimePanel;
     private JLabel tittleCreditsPanel;
-    private JLabel titleKeysPanel;
     private JLabel titleMainViewPanel;
     private JPanel containerFoldersPanel;
     private JTextPane textFoldersPanel;
@@ -46,16 +47,6 @@ public class SettingsWindow extends JFrame {
     private JComboBox<Integer> creditsComboBox;
     private JCheckBox blockScreenWhenHavenCheckBox;
     private JCheckBox saveSongsWhenPowerCheckBox;
-    private JTextPane textKeysPanel;
-    private JButton upSongButton;
-    private JButton downSongButton;
-    private JButton upGenderButton;
-    private JButton downGenderButton;
-    private JButton addCoinButton;
-    private JButton removeCoinButton;
-    private JButton powerOffButton;
-    private JButton nextSongButton;
-    private JPanel containerKeysPanel;
     private JPanel containerCreditsPanel;
     private JPanel containerMainViewPanel;
     private JTextPane textViewPanel;
@@ -70,33 +61,11 @@ public class SettingsWindow extends JFrame {
     private JLabel timeSongLabel;
     private JLabel timeLimitLabel;
     private JLabel creditsLabel;
-    private JLabel upSongLabel;
-    private JLabel upGenderLabel;
-    private JLabel addCoinLabel;
-    private JLabel powerOffLabel;
-    private JLabel downSongLabel;
-    private JLabel downGenderLabel;
-    private JLabel removeCoinLabel;
-    private JLabel nextSongLabel;
     private JLabel backgroundColorMainTextLabel;
     private JLabel backgroundColorMainViewLabel;
     private JLabel fontLabel;
     private JLabel foregroundColorMainTextLabel;
     private JLabel fontSizeLabel;
-    private JLabel upSongSymbolLabel;
-    private JLabel downSongSymbolLabel;
-    private JLabel upGenderSymbolLabel;
-    private JLabel downGenderSymbolLabel;
-    private JLabel removeCoinSymbolLabel;
-    private JLabel nextSongSymbolLabel;
-    private JLabel powerOffSymbolLabel;
-    private JLabel addCoinSymbolLabel;
-    private JButton downSongsButton;
-    private JButton upSongsButton;
-    private JLabel upSongsLabel;
-    private JLabel upSongsSymbolLabel;
-    private JLabel downSongsSymbolLabel;
-    private JLabel downSongsLabel;
     private JLabel fontStyleLabel;
     private JComboBox<String> fontStyleComboBox;
     private JButton exitButton;
@@ -104,11 +73,20 @@ public class SettingsWindow extends JFrame {
     private JPanel listViewPanel;
     private JPanel containerListViewPanel;
     private JLabel titleListViewPanel;
+    private JTextField upSongJTextField;
 
-    // Fonts
-    protected static final String[] fontFamilyNames = graphicsEnvironment.getAvailableFontFamilyNames();
+    private ComboBoxModel<String> modelFonts;
 
     public SettingsWindow() {
+
+        this.setTitle("Settings");
+        this.setMinimumSize(new Dimension(RESOLUTION_WIDTH / 2, RESOLUTION_HEIGHT / 2));
+        this.setLocationRelativeTo(null);
+        this.setResizable(false);
+        this.setDefaultCloseOperation(WindowConstants.DISPOSE_ON_CLOSE);
+        this.getContentPane().setLayout(new BorderLayout(0, 0));
+        this.getContentPane().add(containerPanel);
+        this.setVisible(false);
 
         initComponents();
 
@@ -226,158 +204,6 @@ public class SettingsWindow extends JFrame {
                 promotionalSearchButton.setEnabled(promotionalVideoCheckBox.isSelected());
             }
         });
-        upSongButton.addActionListener(new ActionListener() {
-            /**
-             * Invoked when an action occurs.
-             *
-             * @param e the event to be processed
-             */
-            @Override
-            public void actionPerformed(ActionEvent e) {
-                JDialog dialog = keySelector();
-                dialog.addKeyListener(new KeyAdapter() {
-                    @Override
-                    public void keyPressed(KeyEvent e) {
-                        upSongSymbolLabel.setText(KeyEvent.getKeyText(e.getKeyCode()));
-                        upSongButton.setText(Integer.toString(e.getKeyCode()));
-                        dialog.dispose();
-                    }
-                });
-            }
-        });
-        downSongButton.addActionListener(new ActionListener() {
-            /**
-             * Invoked when an action occurs.
-             *
-             * @param e the event to be processed
-             */
-            @Override
-            public void actionPerformed(ActionEvent e) {
-                JDialog dialog = keySelector();
-                dialog.addKeyListener(new KeyAdapter() {
-                    @Override
-                    public void keyPressed(KeyEvent e) {
-                        downSongSymbolLabel.setText(KeyEvent.getKeyText(e.getKeyCode()));
-                        downSongButton.setText(Integer.toString(e.getKeyCode()));
-                        dialog.dispose();
-                    }
-                });
-            }
-        });
-        upGenderButton.addActionListener(new ActionListener() {
-            /**
-             * Invoked when an action occurs.
-             *
-             * @param e the event to be processed
-             */
-            @Override
-            public void actionPerformed(ActionEvent e) {
-                JDialog dialog = keySelector();
-                dialog.addKeyListener(new KeyAdapter() {
-                    @Override
-                    public void keyPressed(KeyEvent e) {
-                        upGenderSymbolLabel.setText(KeyEvent.getKeyText(e.getKeyCode()));
-                        upGenderButton.setText(Integer.toString(e.getKeyCode()));
-                        dialog.dispose();
-                    }
-                });
-            }
-        });
-        downGenderButton.addActionListener(new ActionListener() {
-            /**
-             * Invoked when an action occurs.
-             *
-             * @param e the event to be processed
-             */
-            @Override
-            public void actionPerformed(ActionEvent e) {
-                JDialog dialog = keySelector();
-                dialog.addKeyListener(new KeyAdapter() {
-                    @Override
-                    public void keyPressed(KeyEvent e) {
-                        downSongSymbolLabel.setText(KeyEvent.getKeyText(e.getKeyCode()));
-                        downGenderButton.setText(Integer.toString(e.getKeyCode()));
-                        dialog.dispose();
-                    }
-                });
-            }
-        });
-        addCoinButton.addActionListener(new ActionListener() {
-            /**
-             * Invoked when an action occurs.
-             *
-             * @param e the event to be processed
-             */
-            @Override
-            public void actionPerformed(ActionEvent e) {
-                JDialog dialog = keySelector();
-                dialog.addKeyListener(new KeyAdapter() {
-                    @Override
-                    public void keyPressed(KeyEvent e) {
-                        addCoinSymbolLabel.setText(KeyEvent.getKeyText(e.getKeyCode()));
-                        addCoinButton.setText(Integer.toString(e.getKeyCode()));
-                        dialog.dispose();
-                    }
-                });
-            }
-        });
-        removeCoinButton.addActionListener(new ActionListener() {
-            /**
-             * Invoked when an action occurs.
-             *
-             * @param e the event to be processed
-             */
-            @Override
-            public void actionPerformed(ActionEvent e) {
-                JDialog dialog = keySelector();
-                dialog.addKeyListener(new KeyAdapter() {
-                    @Override
-                    public void keyPressed(KeyEvent e) {
-                        removeCoinSymbolLabel.setText(KeyEvent.getKeyText(e.getKeyCode()));
-                        removeCoinButton.setText(Integer.toString(e.getKeyCode()));
-                        dialog.dispose();
-                    }
-                });
-            }
-        });
-        powerOffButton.addActionListener(new ActionListener() {
-            /**
-             * Invoked when an action occurs.
-             *
-             * @param e the event to be processed
-             */
-            @Override
-            public void actionPerformed(ActionEvent e) {
-                JDialog dialog = keySelector();
-                dialog.addKeyListener(new KeyAdapter() {
-                    @Override
-                    public void keyPressed(KeyEvent e) {
-                        powerOffSymbolLabel.setText(KeyEvent.getKeyText(e.getKeyCode()));
-                        powerOffButton.setText(Integer.toString(e.getKeyCode()));
-                        dialog.dispose();
-                    }
-                });
-            }
-        });
-        nextSongButton.addActionListener(new ActionListener() {
-            /**
-             * Invoked when an action occurs.
-             *
-             * @param e the event to be processed
-             */
-            @Override
-            public void actionPerformed(ActionEvent e) {
-                JDialog dialog = keySelector();
-                dialog.addKeyListener(new KeyAdapter() {
-                    @Override
-                    public void keyPressed(KeyEvent e) {
-                        nextSongSymbolLabel.setText(KeyEvent.getKeyText(e.getKeyCode()));
-                        nextSongButton.setText(Integer.toString(e.getKeyCode()));
-                        dialog.dispose();
-                    }
-                });
-            }
-        });
         exitButton.addActionListener(new ActionListener() {
             /**
              * Invoked when an action occurs.
@@ -393,14 +219,7 @@ public class SettingsWindow extends JFrame {
 
     private void initComponents() {
 
-        this.setTitle("Settings");
-        this.setMinimumSize(new Dimension(RESOLUTION_WIDTH / 2, RESOLUTION_HEIGHT / 2));
-        this.setLocationRelativeTo(null);
-        this.setResizable(false);
-        this.setDefaultCloseOperation(WindowConstants.DISPOSE_ON_CLOSE);
-        this.getContentPane().setLayout(new BorderLayout(0, 0));
-        this.getContentPane().add(containerPanel);
-        this.setVisible(false);
+        modelFonts = new DefaultComboBoxModel<>(fontFamilyNames);
 
         paintComponents();
 
@@ -410,7 +229,6 @@ public class SettingsWindow extends JFrame {
 
     private void paintComponents() {
 
-        ComboBoxModel<String> modelFonts = new DefaultComboBoxModel<>(fontFamilyNames);
         fontComboBox.setModel(modelFonts);
 
     }
@@ -438,37 +256,6 @@ public class SettingsWindow extends JFrame {
         creditsComboBox.setSelectedIndex(getAmountCredits());
         blockScreenWhenHavenCheckBox.setSelected(isLockScreen());
         saveSongsWhenPowerCheckBox.setSelected(isSaveSongs());
-
-        // Keys control section
-        upSongSymbolLabel.setText(KeyEvent.getKeyText(getValueToUpIndex()));
-        upSongButton.setText(Integer.toString(getValueToUpIndex()));
-
-        downSongSymbolLabel.setText(KeyEvent.getKeyText(getValueToDownIndex()));
-        downSongButton.setText(Integer.toString(getValueToDownIndex()));
-
-        upSongsSymbolLabel.setText(KeyEvent.getKeyText(getValueToUpIndexes()));
-        upSongsButton.setText(Integer.toString(getValueToUpIndexes()));
-
-        downSongsSymbolLabel.setText(KeyEvent.getKeyText(getValueToDownIndexes()));
-        downSongsButton.setText(Integer.toString(getValueToDownIndexes()));
-
-        upGenderSymbolLabel.setText(KeyEvent.getKeyText(getValueToChangeGenderToUp()));
-        upGenderButton.setText(Integer.toString(getValueToChangeGenderToUp()));
-
-        downGenderSymbolLabel.setText(KeyEvent.getKeyText(getValueToChangeGenderToDown()));
-        downGenderButton.setText(Integer.toString(getValueToChangeGenderToDown()));
-
-        addCoinSymbolLabel.setText(KeyEvent.getKeyText(getValueToAddCoin()));
-        addCoinButton.setText(Integer.toString(getValueToAddCoin()));
-
-        removeCoinSymbolLabel.setText(KeyEvent.getKeyText(getValueToRemoveCoin()));
-        removeCoinButton.setText(Integer.toString(getValueToRemoveCoin()));
-
-        powerOffSymbolLabel.setText(KeyEvent.getKeyText(getValueToPowerOff()));
-        powerOffButton.setText(Integer.toString(getValueToPowerOff()));
-
-        nextSongSymbolLabel.setText(KeyEvent.getKeyText(getValueToPlayNextSong()));
-        nextSongButton.setText(Integer.toString(getValueToPlayNextSong()));
 
         // Main view control section
         backgroundColorMainViewLabel.setBackground(Color.decode(getValueBackgroundColor()));
@@ -498,18 +285,6 @@ public class SettingsWindow extends JFrame {
         properties.put(KEY_LOCK_SCREEN, String.valueOf(blockScreenWhenHavenCheckBox.isSelected()));
         properties.put(KEY_SAVE_SONGS, String.valueOf(saveSongsWhenPowerCheckBox.isSelected()));
 
-        //Keys
-        properties.put(KEY_UP_SONG, upSongButton.getText());
-        properties.put(KEY_DOWN_SONG, downSongButton.getText());
-        properties.put(KEY_UP_SONGS, upSongsButton.getText());
-        properties.put(KEY_DOWN_SONGS, downSongsButton.getText());
-        properties.put(KEY_UP_GENDER, upGenderButton.getText());
-        properties.put(KEY_DOWN_GENDER, downGenderButton.getText());
-        properties.put(KEY_ADD_COIN, addCoinButton.getText());
-        properties.put(KEY_REMOVE_COIN, removeCoinButton.getText());
-        properties.put(KEY_POWER_OFF, powerOffButton.getText());
-        properties.put(KEY_NEXT_SONG, nextSongButton.getText());
-
         // Main view settings
         properties.put(KEY_BACKGROUND_COLOR, "#" + Integer.toHexString(backgroundColorMainViewLabel.getBackground().getRGB()).substring(2).toUpperCase());
         properties.put(KEY_FOREGROUND, "#" + Integer.toHexString(foregroundColorViewLabel.getBackground().getRGB()).substring(2).toUpperCase());
@@ -519,17 +294,6 @@ public class SettingsWindow extends JFrame {
 
         return properties;
 
-    }
-
-    private JDialog keySelector() {
-        JDialog dialog = new JDialog();
-
-        dialog.setTitle("Select Key");
-        dialog.setLocationRelativeTo(this);
-        dialog.setMinimumSize(new Dimension(RESOLUTION_WIDTH / 8, RESOLUTION_HEIGHT / 8));
-        dialog.setVisible(true);
-
-        return dialog;
     }
 
 }
